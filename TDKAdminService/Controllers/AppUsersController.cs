9d749c87e2d03f8d23f523949d3ec4d4e63d6c31
jakuboles <koles000@o2.pl>
2020-04-25 17:58:27 +0200

    Add project files.

---
+++ TDKAdminService/Controllers/AppUsersController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TDKAdminService.Models;
+
+namespace TDKAdminService.Controllers
+{
+    public class AppUsersController : Controller
+    {
+        private Entities db = new Entities();
+
+        // GET: AppUsers
+        public ActionResult Index()
+        {
+            return View(db.AppUser.ToList());
+        }
+
+        // GET: AppUsers/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            AppUser appUser = db.AppUser.Find(id);
+            if (appUser == null)
+            {
+                return HttpNotFound();
+            }
+            return View(appUser);
+        }
+
+        // GET: AppUsers/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            AppUser appUser = db.AppUser.Find(id);
+            if (appUser == null)
+            {
+                return HttpNotFound();
+            }
+            return View(appUser);
+        }
+
+        // POST: AppUsers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            AppUser appUser = db.AppUser.Find(id);
+            db.AppUser.Remove(appUser);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ TDKAdminService/Controllers/PlacesController.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TDKAdminService.Models;
+
+namespace TDKAdminService.Controllers
+{
+    public class PlacesController : Controller
+    {
+        private Entities db = new Entities();
+
+        // GET: Places
+        public ActionResult Index()
+        {
+            var places = db.Place.ToList();
+
+            foreach (var item in places)
+            {
+                item.createdAt = item.createdAt.LocalDateTime;
+                item.updatedAt = item.updatedAt.LocalDateTime;
+            }
+
+            return View(places);
+        }
+
+        // GET: Places/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Place place = db.Place.Find(id);
+            if (place == null)
+            {
+                return HttpNotFound();
+            }
+            return View(place);
+        }
+
+        // GET: Places/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Places/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,placeName,placeDescription,latitude,longitude,PlaceAddess,version,createdAt,updatedAt")] Place place)
+        {
+            if (ModelState.IsValid)
+            {
+                place.createdAt = DateTime.Now;
+                place.updatedAt = DateTime.Now;
+                place.id = Guid.NewGuid().ToString();
+                db.Place.Add(place);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(place);
+        }
+
+        // GET: Places/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Place place = db.Place.Find(id);
+            if (place == null)
+            {
+                return HttpNotFound();
+            }
+            return View(place);
+        }
+
+        // POST: Places/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,placeName,placeDescription,latitude,longitude,PlaceAddess,version,createdAt,updatedAt")] Place place)
+        {
+            if (ModelState.IsValid)
+            {
+                place.createdAt = place.createdAt;
+                place.updatedAt = DateTime.Now;
+                db.Entry(place).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(place);
+        }
+
+        // GET: Places/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Place place = db.Place.Find(id);
+            if (place == null)
+            {
+                return HttpNotFound();
+            }
+            return View(place);
+        }
+
+        // POST: Places/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            Place place = db.Place.Find(id);
+            db.Place.Remove(place);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
